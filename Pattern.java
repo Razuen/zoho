df11756e03b55cd86a3106ab6299b0aa93da70c4
Razuen <32031785+Razuen@users.noreply.github.com>
2021-02-12 09:44:22 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+/**
+ * Pattern
+ */
+public class Pattern {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        int stringLength = word.length();
+        int midPos = stringLength / 2;
+        char mid = word.charAt(midPos);
+
+        for (int i = 0, j = 0; i < stringLength && j < midPos; i++) {
+
+        }
+
+    }
+
+}
